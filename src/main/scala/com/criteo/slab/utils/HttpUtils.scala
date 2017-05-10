496102a437b4012c0e32ec6edebbf4b64654af56
Sheng Ran <s.ran@criteo.com>
2017-05-10 12:06:04 +0200

    Improve error handling in HttpUtils
    - Better observability with relevant logs

--- src/main/scala/com/criteo/slab/utils/HttpUtils.scala
+++ src/main/scala/com/criteo/slab/utils/HttpUtils.scala
@@ -7,6 +7,7 @@ import lol.http._
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 /** Http utilities
   *
@@ -16,9 +17,9 @@ object HttpUtils {
 
   /** Send a GET request
     *
-    * @param url The URL
+    * @param url     The URL
     * @param headers The request headers
-    * @param ec The [[ExecutionContext]]
+    * @param ec      The [[ExecutionContext]]
     * @tparam A The result type which should provide a content decoder implementation
     * @return The result wrapped in [[Future]]
     */
@@ -29,29 +30,23 @@ object HttpUtils {
     val path = url.getPath + Option(url.getQuery).map("?" + _).getOrElse("")
     val port = if (url.getPort > 0) url.getPort else url.getDefaultPort
     val request = Get(path).addHeaders(defaultHeaders ++ headers)
-    val completeURL = s"${url.getProtocol}://${url.getHost}:${port}$path"
-    logger.info(s"Requesting $completeURL")
+    val fullURL = s"${url.getProtocol}://${url.getHost}:${port}$path"
+    logger.info(s"Requesting $fullURL")
     val start = Instant.now
     Client(url.getHost, port, url.getProtocol) runAndStop { client =>
       client.run(request) { res =>
-        logger.info(s"Response from $completeURL, status: ${res.status}, ${Instant.now.toEpochMilli - start.toEpochMilli}ms")
-        if (res.status < 400)
-          res.readAs[A]
-        else
-          Future.failed(FailedRequestException(res))
-      } recoverWith {
-        case e =>
-          logger.error(s"Request $completeURL failed: ${e.getMessage}")
-          Future.failed(e)
-      }
+        logger.info(s"Response from $fullURL, status: ${res.status}, ${Instant.now.toEpochMilli - start.toEpochMilli}ms")
+        handleResponse(res, fullURL)
+      } recoverWith handleError(fullURL)
     }
   }
 
   /** Make a HTTP Get client of which the connection is kept open
     *
     * Do not use this until lolhttp client leak issue is resolved
+    *
     * @param url The URL
-    * @param ec The [[ExecutionContext]]
+    * @param ec  The [[ExecutionContext]]
     * @return The client with which a GET request can be sent
     */
   def makeGet(url: URL)(implicit ec: ExecutionContext): SafeHTTPGet = {
@@ -72,25 +67,52 @@ object HttpUtils {
 
   private def encodeURI(in: String) = URLEncoder.encode(in, "UTF-8").replace("+", "%20")
 
+  private def handleResponse[A: ContentDecoder](res: Response, url: String)(implicit ec: ExecutionContext): Future[A] = {
+    if (res.status < 400)
+      res.readAs[A]
+    else
+      res
+        .readAs[String]
+        .recoverWith { case e =>
+          logger.error(e.getMessage, e)
+          Future.successful("Unable to get the message")
+        }
+        .flatMap { message =>
+          logger.info(s"Request to $url has failed, status: ${res.status}, message: $message")
+          Future.failed(FailedRequestException(res))
+        }
+  }
+
+  private def handleError[A](url: String)(implicit ec: ExecutionContext): PartialFunction[Throwable, Future[A]] = {
+    case f: FailedRequestException => Future.failed(f)
+    case NonFatal(e) =>
+      logger.error(s"Error when requesting $url: ${e.getMessage}", e)
+      Future.failed(e)
+  }
+
+  /** A response with status >= 400
+    *
+    * @param response The [[Response]]
+    */
   case class FailedRequestException(response: Response) extends Exception
 
+  /**
+    * It be used to send GET requests using the same client
+    *
+    * @param client         The client
+    * @param defaultHeaders Default request headers
+    * @param ec             [[ExecutionContext]]
+    */
   case class SafeHTTPGet(client: Client, defaultHeaders: Map[HttpString, HttpString])(implicit ec: ExecutionContext) {
     def apply[A: ContentDecoder](path: String, headers: Map[HttpString, HttpString] = Map.empty): Future[A] = {
-      val url = s"${client.scheme}://${client.host}:${client.port}$path"
+      val fullURL = s"${client.scheme}://${client.host}:${client.port}$path"
       val request = Get(path).addHeaders(defaultHeaders ++ headers)
-      logger.info(s"Requesting $url")
+      logger.info(s"Requesting $fullURL")
       val start = Instant.now
-      client.run(request) { res =>
-        logger.info(s"Response from $url, status: ${res.status}, ${Instant.now.toEpochMilli - start.toEpochMilli}ms")
-        if (res.status < 400)
-          res.readAs[A]
-        else
-          Future.failed(FailedRequestException(res))
-      } recoverWith {
-        case e =>
-          logger.error(s"Request $url failed: ${e.getMessage}")
-          Future.failed(e)
-      }
+      client.run(request) { res: Response =>
+        logger.info(s"Response from $fullURL, status: ${res.status}, ${Instant.now.toEpochMilli - start.toEpochMilli}ms")
+        handleResponse(res, fullURL)
+      } recoverWith handleError(fullURL)
     }
   }
 
